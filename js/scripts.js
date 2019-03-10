7d681d5e1fcb12c739ad129720191f61d9afab24
Karla Carballo <kacarballo@gmail.com>
2019-03-10 12:47:19 -0700

    added functionality across the board!

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 // business logic
-function getDisneyCharacter(color, quote, word) {
-  if(color == "Red" || "Black" || "Yellow" && quote == "quote1")
+function getDisneyCharacter(color, quote, noun) {
+  if(color == "red" || "black" || "yellow" && quote == "quote1" && noun == "playful")
     $("#mickey").show();
-  else if(color == "Blue" || "Yellow" && quote === "quote2" && noun == "Spaz")
+  else if(color == "blue" || "yellow" && quote == "quote2" && noun == "spaz" || "sassy")
     $("#dory").show();
-  // else if(color)
-
+  else if(color == "yellow" || "red" || "orange" && quote == "quote3" && noun == "cuddly")
+    $("#winnie").show();
+  else if(color == "blue" || "red" || "green" && quote == "quote4" && noun == "optimistic")
+    $("#moana").show();
+  else if(color == "yellow" || "red" || "orange" && quote == "quote5" && noun == "regal" || "sassy")
+    $("#mufasa").show();
+  else if(color == "blue" || "white" || "orange" && quote == "quote6" && noun == "spaz" || "sassy")
+    $("#olaf").show();
 }
 
 // user interface logic
@@ -17,7 +23,7 @@ $(document).ready(function() {
     var quote = $('input[name=quote]:checked').val();
     // var place = $('input[name=quote]:checked').val();
     var noun = $("#noun option:selected").text();
-    // alert("name = " + name + " color = " + color + " quote = " + quote + " word = " + wordDescribe);
+    alert("name = " + name + " color = " + color + " quote = " + quote + " word = " + wordDescribe);
 
     getDisneyCharacter(color, quote, wordDescribe);
     event.preventDefault();
